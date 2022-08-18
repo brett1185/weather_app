8019e9bdab727371210db08285b2869e8d605359
Brett <89299506+brett1185@users.noreply.github.com>
2022-08-17 20:47:21 -0400

    Add files via upload

--- weather/app.py
+++ weather/app.py
@@ -1,13 +1,24 @@
 import requests
+from flask import Flask, render_template, redirect, session
+app=Flask(__name__)
 
 api_key='6b4908b8ab6fb4da0b7881624320662f'
 
 input = input('Enter city:')
 
-weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={input}&appid={api_key}')
 
-weather = weather_data.json()['weather'][0]['description']
-temp = weather_data.json()['main']['temp']
-f_temp=round((temp - 273.15) * (9/5) +32)
 
-print(weather, f_temp)
+
+@app.route('/')
+def index():
+    weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={input}&appid={api_key}')
+    weather = weather_data.json()['weather'][0]['description']
+    temp = weather_data.json()['main']['temp']
+    f_temp=round((temp - 273.15) * (9/5) +32)
+    return render_template('display.html', weather=weather, f_temp=f_temp)
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True) 
